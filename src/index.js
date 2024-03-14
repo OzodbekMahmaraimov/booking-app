501b8d7fd4f3a2ee785f5167b33ab3df76f0e9cf
Muhammadyusuf <temirovmuhammadyusuf70@gmail.com>
2024-03-14 15:51:37 +0500

    abdulaziz succcsec

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import "./assets/style.css";
-import { MainHotelDashboard } from "./pages/main-hotel-dashboard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
 	<BrowserRouter>
-		<ResMainItemList />
+		<App />
 	</BrowserRouter>
 );
