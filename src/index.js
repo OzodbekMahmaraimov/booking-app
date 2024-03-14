738eb3cd87833ca24dd64ca27284151eeb842ae3
Muhammadyusuf <temirovmuhammadyusuf70@gmail.com>
2024-03-14 16:38:56 +0500

    asilbek changing succcsec

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./app";
 import "./assets/style.css";
-import { AdminPayment } from "./pages/app/admin/mainAdmin/payment";
-import { HotelProfile } from "./pages/app/admin/hotelAdmin/Profile";
+import { App } from "antd";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
 	<BrowserRouter>
-		< HotelProfile/>
+		<App/>
 	</BrowserRouter>
 );
