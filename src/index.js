1309039ae038f45c2b3aa14e0345ab14b83e567c
Muhammadyusuf <temirovmuhammadyusuf70@gmail.com>
2024-03-14 17:47:27 +0500

    omadbeknikini qushib bumadi

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./assets/style.css";
+// import "./assets/style.css";
 import { App } from "antd";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
 	<BrowserRouter>
-		<App/>
+		<App />
 	</BrowserRouter>
 );
