04ed7cf2173e4483a32e01d2b758a83fed12ac8c
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-03-14 16:29:33 +0500

    finish my code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./app";
 import "./assets/style.css";
 import { AdminPayment } from "./pages/app/admin/mainAdmin/payment";
-import { HotelProfile } from "./pages/app/admin/hotelAdmin/Profile";
+import { HotelProfile } from "./pages/app/admin/retourantAdmin/Profile";
+import { ResProfileAnally } from "./pages/app/restourant/profile/analthy";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
 	<BrowserRouter>
-		< HotelProfile/>
+		<HotelProfile />
 	</BrowserRouter>
 );
