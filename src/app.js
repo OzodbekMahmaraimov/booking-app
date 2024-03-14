d4d288f46fccb77bad599a6b0884a88801019b5e
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-03-14 19:32:12 +0500

    mana

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Main from './components/layout/main'
 import { Route, Routes } from 'react-router-dom'
-import OrderLanding from './pages/app/orderLandingPage'
 import Login from './pages/auth/login'
 import SignUp from './pages/auth/signUp'
 import IndexMain from './pages/main-admin-page'
@@ -13,7 +12,6 @@ const App = () => {
       <Routes>
         <Route path='/' Component={Main} />
         <Route path='/MainDashboardIndex' Component={IndexMain} />
-        <Route path='/order-landing page' Component={OrderLanding} />
         <Route path='/loginSignUp' Component={Login} />
         <Route path='/signUp' Component={SignUp} />
       </Routes>
