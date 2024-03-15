0d15fe4a08c0c73d77bb749d04a32abc65301361
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-03-15 17:31:42 +0500

    iconclassName, icon

--- src/pages/main-hotel-page/components/cards/imgCard.jsx
+++ src/pages/main-hotel-page/components/cards/imgCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 function ImgCard({img, width}) {
   return (
     <div className={`gallary_card w-${width} overflow-hidden shadow-2xl`}>
-        <img className="cards_img" src={img} alt="img"/>
+      <LazyLoadImage className="cards_img" src={img} alt="img"/>
     </div>
   )
 }
