5c60ca6e3a4422f733036b3597d0e15af6b01afb
Muhammadyusuf <temirovmuhammadyusuf70@gmail.com>
2024-03-15 13:59:22 +0500

    sardorbek succsefuly

--- src/pages/main-admin-page/component/mainAdminManage.jsx
+++ src/pages/main-admin-page/component/mainAdminManage.jsx
@@ -1,52 +1,36 @@
-import React from 'react'
-import RegistrationForm from './registerFrom'
-<<<<<<< HEAD
-=======
-import RegisterTable from './registerTable'
->>>>>>> sardorbek/main
+import React from "react";
+import RegistrationForm from "./registerFrom";
+import RegisterTable from "./registerTable";
 
 const MainAdminManager = () => {
-    const interButton = [
-        {
-            name: 'Hotel manager'
-        },
-        {
-            name: 'Restaurant manager'
-        },
-        {
-            name: 'Coffe shop manager'
-        },
+	const interButton = [
+		{
+			name: "Hotel manager",
+		},
+		{
+			name: "Restaurant manager",
+		},
+		{
+			name: "Coffe shop manager",
+		},
+	];
+	return (
+		<div className="w-full h-screen bg-[#FFF] rounded-lg">
+			<div className="flex justify-evenly items-center">
+				{interButton &&
+					interButton.map((res, i) => (
+						<button className="px-10 rounded border-2 border-[#F46A06] mt-10 bg-[#F1E8D7]">
+							{res.name}
+						</button>
+					))}
+			</div>
 
-    ]
-    return (
-<<<<<<< HEAD
-        <div className='w-full h-screen bg-[#FFF] rounded-lg'>
-=======
-        <div className='w-full h-auto bg-[#FFF] rounded-lg'>
->>>>>>> sardorbek/main
-            <div className='flex justify-evenly items-center'>
-                {interButton &&
-                    interButton.map((res, i) =>
-                        < button className='px-10 rounded border-2 border-[#F46A06] mt-10 bg-[#F1E8D7]' >
-                            {res.name}
-                        </button>
-                    )
-                }
-            </div>
-<<<<<<< HEAD
-            <div>
-                <RegistrationForm />
-            </div>
-=======
-            
-            <div>
-                <RegistrationForm />
-                <RegisterTable/>
-            </div>
-            
->>>>>>> sardorbek/main
-        </div >
-    )
-}
+			<div>
+				<RegistrationForm />
+				<RegisterTable />
+			</div>
+		</div>
+	);
+};
 
-export default MainAdminManager
+export default MainAdminManager;
