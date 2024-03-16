f50f0070bdedc8891e2fac9a3afe4439b89bb30f
omadbekolimov <omadbekolimov96@gmail.com>
2024-03-16 15:14:00 +0500

    ketdi

--- src/pages/app/admin/restourantAdmin/category/costumer.jsx
+++ src/pages/app/admin/restourantAdmin/category/costumer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminDasheboard from "./dashboard";
 import home from "../../../../../assets/img/Vector (1).png";
 import line from "../../../../../assets/images/Line 10.png";
@@ -6,102 +6,51 @@ import { Dashboard } from ".";
 import { MainDashboardManageRooms } from "../../../../main-hotel-dashboard/magage-rooms";
 import AdminDashboard from "./dashboard";
 import ResAdminSidebar from "../components/sidebar";
+import axios from "axios";
 
 
    
 export const Customer = () => {
 	const [selectedItems, setSelectedItems] = useState({});
-	const [currentPage, setCurrentPage] = useState(1);
-	const itemsPerPage = 5;
-	const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [customer, setCustomer] = useState(null);
 
 	
 
-	const castTasble = [
-		{
-			id: 1,
-			phone: "+998907748674",
-			name: "Byiringiro Moise",
-			total: "6000Rwf",
-			last: "3000Rwf",
-			send: "...",
-		},
-		{
-			id: 2,
-			phone: "+998907748674",
-			name: "Byiringiro Moise",
-			total: "6000Rwf",
-			last: "3000Rwf",
-			send: "...",
-		},
-		{
-			id: 3,
-			phone: "+998907748674",
-			name: "Byiringiro Moise",
-			total: "6000Rwf",
-			last: "3000Rwf",
-			send: "...",
-		},
-		{
-			id: 4,
-			phone: "+998907748674",
-			name: "Byiringiro Moise",
-			total: "6000Rwf",
-			last: "3000Rwf",
-			send: "...",
-		},
-		{
-			id: 5,
-			phone: "+998907748674",
-			name: "Byiringiro Moise",
-			total: "6000Rwf",
-			last: "3000Rwf",
-			send: "...",
-		},
-		{
-			id: 6,
-			phone: "+998907748674",
-			name: "Byiringiro Moise",
-			total: "6000Rwf",
-			last: "3000Rwf",
-			send: "...",
-		},
-		{
-			id: 6,
-			phone: "+998907748674",
-			name: "Byiringiro Moise",
-			total: "6000Rwf",
-			last: "3000Rwf",
-			send: "...",
-		},
-		{
-			id: 7,
-			phone: "+998907748674",
-			name: "Byiringiro Moise",
-			total: "6000Rwf",
-			last: "3000Rwf",
-			send: "...",
-		},
-	];
+	useEffect(() => {
+        axios.get("http://localhost:3005/manager-dashboard")
+            .then((res) => {
+                setCustomer(res.data.customers);
+				
+                setCurrentPage(1); // Sahifani qayta boshlash
+            })
+            .catch((error) => {
+                console.error("Error fetching data: ", error);
+            });
+    }, []);
+
+	const nextPage = () => {
+        setCurrentPage((prevPage) => prevPage + 1);
+    };
+
+    const prevPage = () => {
+        setCurrentPage((prevPage) => prevPage - 1);
+    };
 
 	
 
 	const lastPageIndex = currentPage * itemsPerPage;
-	const firstPageIndex = lastPageIndex - itemsPerPage;
-	const currentItems = castTasble.slice(firstPageIndex, lastPageIndex);
+    const firstPageIndex = lastPageIndex - itemsPerPage;
+    const currentItems = customer ? customer.slice(firstPageIndex, lastPageIndex) : [];
 
 	
 
 	
 
 	
-	const nextPage = () => {
-		setCurrentPage((prevPage) => prevPage + 1);
-	};
-
-	const prevPage = () => {
-		setCurrentPage((prevPage) => prevPage - 1);
-	};
+	
 	return (
 		<div className="w-full bg-orange-400">
 			<div className="flex">
@@ -122,24 +71,37 @@ export const Customer = () => {
 
 					<div className=" mt-6 ml-4 bg-white rounded-xl border-[10px] border-white-600 px-1 py-4">
 						<table className="mt-4 w-full ml-1 text-center rounded-lg">
-							<tr className="border-2 w-full bg-[#F1E8D7] h-10 ">
-								<th>Customer ID</th>
+							
+							 <thead>
+							 <tr className="border-2 w-full bg-[#F1E8D7] h-10 ">
+							 <th>Customer ID</th>
 								<th>Customer phone</th>
 								<th>Customer Name</th>
 								<th>Total Spent</th>
 								<th>Last order</th>
 								<th></th>
 							</tr>
-							{currentItems.map((item, i) => (
-								<tr key={item.id}>
-									<td className="p-4">{item.id}</td>
-									<td>{item.phone}</td>
-									<td>{item.name}</td>
-									<td>{item.total}</td>
-									<td>{item.last}</td>
-									<td>{item.send}</td>
-								</tr>
-							))}
+							</thead>	
+							<tbody>
+							{currentItems.length > 0 ? (
+                                    currentItems.map((item, i) => (
+                                        <tr key={i} className="border-b border-gray-400">
+                                            <td className="p-4">{item.id}</td>
+                                            <td>{item.phone}</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.spend}</td>
+                                            <td>{item.order}</td>
+                                            <td>{item.status}</td>
+                                        </tr>
+                                    ))
+                                ) : (
+                                    <tr className="border-b border-gray-400">
+                                        <td colSpan="6" className="font-bold text-lg py-3">
+                                            No orders found
+                                        </td>
+                                    </tr>
+                                )}
+							</tbody>
 						</table>
 						<div className="p-2">
 						<button
@@ -151,7 +113,7 @@ export const Customer = () => {
 						</button>
 						<button
 							onClick={nextPage}
-							disabled={currentPage >= Math.ceil(castTasble.length / itemsPerPage)}
+							disabled={currentItems.length < itemsPerPage}
 							className="bg-[#F46A06] m-1 hover:bg-white hover:text-black text-white hover:transition-all border-2 border-[#F46A06] py-2 px-3 rounded-md"
 						>
 							Next
--- src/pages/app/admin/restourantAdmin/category/orderList.jsx
+++ src/pages/app/admin/restourantAdmin/category/orderList.jsx
@@ -1,199 +1,126 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import home from "../../../../../assets/images/Vector (1).png";
 import line from "../../../../../assets/images/Line 10.png";
 import AdminDashboard from "./dashboard";
 import ResAdminSidebar from "../components/sidebar";
-
-
+import axios from "axios";
 
 const Order_LIst = () => {
-	const [selectedItems, setSelectedItems] = useState({});
-	const [currentPage, setCurrentPage] = useState(1);
-	const itemsPerPage = 5;
-	const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedItems, setSelectedItems] = useState({});
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [order, setOrder] = useState(null);
 
-	const navData = [
-		{ id: 1, name: "All Orders" },
-		{ id: 2, name: "New order" },
-		{ id: 3, name: "On Progress" },
-		{ id: 4, name: "Canceled Orders" },
-		{ id: 5, name: "Complete orders" },
-	];
+    const navData = [
+        { id: 1, name: "All Orders" },
+        { id: 2, name: "New order" },
+        { id: 3, name: "On Progress" },
+        { id: 4, name: "Canceled Orders" },
+        { id: 5, name: "Complete orders" },
+    ];
 
-	const tableData = [
-		{
-			id: 1,
-			time: "08:30:12",
-			date: "2022-04-20",
-			name: "Moise Byiringiro",
-			active: "Yes",
-			status: "Pending",
-		},
-		{
-			id: 2,
-			time: "08:30:12",
-			date: "2022-04-20",
-			name: "Moise Byiringiro",
-			active: "Yes",
-			status: "Pending",
-		},
-		{
-			id: 3,
-			time: "08:30:12",
-			date: "2022-04-20",
-			name: "Moise Byiringiro",
-			active: "Yes",
-			status: "Pending",
-		},
-		{
-			id: 1,
-			time: "08:30:12",
-			date: "2022-04-20",
-			name: "Moise Byiringiro",
-			active: "Yes",
-			status: "Pending",
-		},
-		{
-			id: 2,
-			time: "08:30:12",
-			date: "2022-04-20",
-			name: "Moise Byiringiro",
-			active: "Yes",
-			status: "Pending",
-		},
-		{
-			id: 3,
-			time: "08:30:12",
-			date: "2022-04-20",
-			name: "Moise Byiringiro",
-			active: "Yes",
-			status: "Pending",
-		},
-		{
-			id: 4,
-			time: "08:30:12",
-			date: "2022-04-20",
-			name: "Moise Byiringiro",
-			active: "Yes",
-			status: "Pending",
-		},
-		{
-			id: 5,
-			time: "08:30:12",
-			date: "2022-04-20",
-			name: "Moise Byiringiro",
-			active: "Yes",
-			status: "Pending",
-		},
-		{
-			id: 6,
-			time: "08:30:12",
-			date: "2022-04-20",
-			name: "Moise Byiringiro",
-			active: "Yes",
-			status: "Pending",
-		},
-	];
+    useEffect(() => {
+        axios.get("http://localhost:3005/manager-dashboard")
+            .then((res) => {
+                setOrder(res.data.orderlist);
+                setCurrentPage(1); // Sahifani qayta boshlash
+            })
+            .catch((error) => {
+                console.error("Error fetching data: ", error);
+            });
+    }, []);
 
+    const nextPage = () => {
+        setCurrentPage((prevPage) => prevPage + 1);
+    };
 
-	const lastPageIndex = currentPage * itemsPerPage;
-	const firstPageIndex = lastPageIndex - itemsPerPage;
-	const currentItems = tableData.slice(firstPageIndex, lastPageIndex);
+    const prevPage = () => {
+        setCurrentPage((prevPage) => prevPage - 1);
+    };
 
-	const toggleSelectAll = (e) => {
-		const newSelectedItems = {};
-		if (e.target.checked) {
-			currentItems.forEach((item) => {
-				newSelectedItems[item.id] = true;
-			});
-		}
-		setSelectedItems(newSelectedItems);
-	};
+    const lastPageIndex = currentPage * itemsPerPage;
+    const firstPageIndex = lastPageIndex - itemsPerPage;
+    const currentItems = order ? order.slice(firstPageIndex, lastPageIndex) : [];
 
-	const toggleSelectItem = (id) => {
-		setSelectedItems((prevItems) => ({
-			...prevItems,
-			[id]: !prevItems[id],
-		}));
-	};
+    return (
+        <div className="w-full">
+            <div className="flex  w-full bg-orange-400">
+                <ResAdminSidebar className="w-[25%]" />
+                <div className="w-[70%]">
+                    <div className="flex ml-4 w-full">
+                        <img className="w-10 h-10 mt-20 ml-4" src={home} alt="" />
+                        <img className="w-5 h-12 mt-20 ml-2" src={line} alt="" />
+                        <h1 className="mt-[100px] ml-1 font-bold text-xl">Restaurant Manager Dashboard</h1>
+                        <div className="flex items-center"></div>
+                        <div className="flex flex-col-reverse w-1/4 ml-[200px]">
+                            <input className="rounded-full w-full h-8 border-2 border-white " type="text" />
+                        </div>
+                        <div className="flex flex-col-reverse ml-2 ">
+                            <button className="font-bold rounded-full border-4 border-white w-20">Search</button>
+                        </div>
+                    </div>
 
-	
-	const nextPage = () => {
-		setCurrentPage((prevPage) => prevPage + 1);
-	};
+                    <div className=" mt-6 ml-4 bg-white rounded-xl border-[10px] border-white-600 px-2 py-2">
+                        {navData.map((item, i) => (
+                            <button key={item.id} className="border-2 border-orange-500 px-3 py-1 rounded-lg m-3">
+                                {item.name}
+                            </button>
+                        ))}
 
-	const prevPage = () => {
-		setCurrentPage((prevPage) => prevPage - 1);
-	};
-	return (
-		<div className="w-full">
-			<div className="flex  w-full bg-orange-400">
-				<ResAdminSidebar className="w-[25%]" />
-				<div className="w-[70%]">
-					<div className="flex ml-4 w-full">
-						<img className="w-10 h-10 mt-20 ml-4" src={home} alt="" />
-						<img className="w-5 h-12 mt-20 ml-2" src={line} alt="" />
-						<h1 className="mt-[100px] ml-1 font-bold text-xl">Restaurant Manager Dashboard</h1>
-						<div className="flex items-center"></div>
-						<div className="flex flex-col-reverse w-1/4 ml-[200px]">
-							<input className="rounded-full w-full h-8 border-2 border-white " type="text" />
-						</div>
-						<div className="flex flex-col-reverse ml-2 ">
-							<button className="font-bold rounded-full border-4 border-white w-20">Search</button>
-						</div>
-					</div>
-
-					<div className=" mt-6 ml-4 bg-white rounded-xl border-[10px] border-white-600 px-2 py-2">
-						{navData.map((item, i) => (
-							<button key={item.id} className="border-2 border-orange-500 px-3 py-1 rounded-lg m-3">
-								{item.name}
-							</button>
-						))}
-
-						<table className="mt-4 w-full ml-1 text-center rounded-lg">
-							<tr className="border-2 w-full bg-[#F1E8D7] h-10 ">
-								<th>OrderId</th>
-								<th>Pick Up Time</th>
-								<th>Pick Up Date</th>
-								<th>Customer Name</th>
-								<th>Ordered</th>
-								<th>Payment Status</th>
-							</tr>
-
-							{currentItems.map((item, i) => (
-								<tr className="border-b border-gray-400">
-									<td className="p-4" key={item.id}>
-										{item.id}
-									</td>
-									<td>{item.time}</td>
-									<td>{item.date}</td>
-									<td>{item.name}</td>
-									<td>{item.active}</td>
-									<td className="text-orange-500 font-bold">{item.status}</td>
-								</tr>
-							))}
-						</table>
-						<div className="p-2">
-						<button
-							onClick={ prevPage }
-							disabled={currentPage === 1}
-							className="bg-[#F46A06] m-1 hover:bg-white hover:text-black text-white hover:transition-all border-2 border-[#F46A06] py-2 px-3 rounded-md"
-						>
-							Previous
-						</button>
-						<button
-							onClick={nextPage}
-							disabled={currentPage >= Math.ceil(tableData.length / itemsPerPage)}
-							className="bg-[#F46A06] m-1 hover:bg-white hover:text-black text-white hover:transition-all border-2 border-[#F46A06] py-2 px-3 rounded-md"
-						>
-							Next
-						</button>
-					</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                        <table className="mt-4 w-full ml-1 text-center rounded-lg">
+                            <thead>
+                                <tr className="border-2 w-full bg-[#F1E8D7] h-10 ">
+                                    <th>OrderId</th>
+                                    <th>Pick Up Time</th>
+                                    <th>Pick Up Date</th>
+                                    <th>Customer Name</th>
+                                    <th>Ordered</th>
+                                    <th>Payment Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {currentItems.length > 0 ? (
+                                    currentItems.map((item, i) => (
+                                        <tr key={i} className="border-b border-gray-400">
+                                            <td className="p-4">{item.orderId}</td>
+                                            <td>{item.time}</td>
+                                            <td>{item.date}</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.ordered}</td>
+                                            <td className="text-orange-500 font-bold">{item.status}</td>
+                                        </tr>
+                                    ))
+                                ) : (
+                                    <tr className="border-b border-gray-400">
+                                        <td colSpan="6" className="font-bold text-lg py-3">
+                                            No orders found
+                                        </td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                        <div className="p-2">
+                            <button
+                                onClick={prevPage}
+                                disabled={currentPage === 1}
+                                className="bg-[#F46A06] m-1 hover:bg-white hover:text-black text-white hover:transition-all border-2 border-[#F46A06] py-2 px-3 rounded-md"
+                            >
+                                Previous
+                            </button>
+                            <button
+                                onClick={nextPage}
+                                disabled={currentItems.length < itemsPerPage}
+                                className="bg-[#F46A06] m-1 hover:bg-white hover:text-black text-white hover:transition-all border-2 border-[#F46A06] py-2 px-3 rounded-md"
+                            >
+                                Next
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Order_LIst;
