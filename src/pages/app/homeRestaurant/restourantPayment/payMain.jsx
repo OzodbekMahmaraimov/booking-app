b36780eea4225db805ac048b48684e65eed24023
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-03-20 14:34:39 +0500

    menimcha navbar full sozlandi

--- src/pages/app/homeRestaurant/restourantPayment/payMain.jsx
+++ src/pages/app/homeRestaurant/restourantPayment/payMain.jsx
@@ -7,11 +7,11 @@ import { FiLogIn } from "react-icons/fi";
 
 
 const navdata = [
-	{ id: 1, icon: <FaHome color='black' />, name: 'Home', to: "/" },
-	{ id: 4, icon: <FiLogIn color='black' />, name: 'Restaurant', to: "/homerestourant" },
-	{ id: 5, icon: <FiLogIn color='black' />, name: 'hotel', to: "/hotel" },
-	{ id: 3, icon: <IoCall color='black' />, name: 'ContactUs', to: "" },
-	{ id: 7, icon: <FiLogIn color='black' />, name: 'Login', to: "loginSignUp" }
+	{ id: 1, icon: <FaHome color='black' />, name: 'Home', to: "/", isActive: false },
+	{ id: 4, icon: <FiLogIn color='black' />, name: 'Restaurant', to: "/homerestourant", isActive: true },
+	{ id: 5, icon: <FiLogIn color='black' />, name: 'hotel', to: "/hotel", isActive: false },
+	{ id: 3, icon: <IoCall color='black' />, name: 'ContactUs', to: "", isActive: false },
+	{ id: 7, icon: <FiLogIn color='black' />, name: 'Login', to: "loginSignUp", isActive: false }
   ]
 function ResPayMain() {
 	return (
