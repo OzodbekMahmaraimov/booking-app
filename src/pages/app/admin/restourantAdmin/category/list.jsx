f75e364145f5f580cb68681650859153ad43d8e8
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-03-20 12:13:18 +0500

    restarant dash board finished

--- src/pages/app/admin/restourantAdmin/category/list.jsx
+++ src/pages/app/admin/restourantAdmin/category/list.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+// ----- ****** images ******* ------ //
 import homeIcon from "./../../../../../assets/images/Vector (1).png";
 import blackLine from "./../../../../../assets/images/Line 10.png";
 import menuIcon from "./../../../../../assets/images/svg.svg";
@@ -20,6 +21,8 @@ const ItemList = () => {
 	const [searchTerm, setSearchTerm] = useState("");
 	const [showModal, setShowModal] = useState(false);
 	const [selectedItems, setSelectedItem] = useState(0);
+	const [item, setItem] = useState([]);
+	const [itemid, setItemid] = useState(0);
 
 
 	const handleAddItem = () => {
@@ -30,21 +33,20 @@ const ItemList = () => {
 		setShowModal(false); // Modalni yopish
 	};
 
-	const handleSearchChange = (event) => {
-		setSearchTerm(event.target.value.toLowerCase());
-	};
+
 
 	useEffect(() => {
 		fetchData();
 	}, []);
 
+
+	// ----- ******* get data ******* ----- //
 	const fetchData = async () => {
 		try {
 			const response = await axios.get(apiUrl + "admin/");
 			const apiItems = response.data;
-			setItems(apiItems); // Yangilangan ro'yxatni saqlash
-			console.log(items);
-
+			setItems(apiItems);
+			setItemid(apiItems.length);
 		} catch (error) {
 			setError("An error occurred while fetching the items.");
 		} finally {
@@ -52,70 +54,37 @@ const ItemList = () => {
 		}
 	};
 
-
-
-	// ------- ****** filter items ********* ------ //
-
-	const filteredItems =
-		searchTerm.length > 0
-			? items.filter(
-				(item) =>
-					item.name.toLowerCase().includes(searchTerm) ||
-					item.category.toLowerCase().includes(searchTerm)
-			)
-			: items;
-
-	const lastPageIndex = currentPage * itemsPerPage;
-	const firstPageIndex = lastPageIndex - itemsPerPage;
-	const currentItems = filteredItems.slice(firstPageIndex, lastPageIndex);
-
-
-	// ------ ******** delet items ******* ------ 
+	// ------ ******** delet items ******* ------ //
 	const handleDeleteSelectedItems = () => {
-		// const remainingItems = items.filter(item => !selectedItems[item.id]);
-		// setItems(remainingItems); // Yangilangan ro'yxatni saqlash
 
-
-
-		// axios.delete(apiUrl + "admin/", deletedItems)
-		// 	.then((res) => {
-		// 		console.log(res);
-		// 	}).catch((err) => {
-		// 		console.error(err);
-		// 	})
-		axios.delete(`${apiUrl}admin/${selectedItems}`,)
-			.then((response) => {
-				console.log('Serverdan qaytgan javob:', response.data);
-			})
-			.catch((error) => {
-				console.error('Xatolik yuz berdi:', error);
-			});
-
-
-		// O'chirilgan elementlarni aniqlash
-
-		// // O'chirilgan elementlarni localStorage'ga saqlash
-		// const existingDeletedItems = JSON.parse(localStorage.getItem('deletedItems') || '[]');
-		// const updatedDeletedItems = [...existingDeletedItems, ...deletedItems.map(item => item.id)];
-		// localStorage.setItem('deletedItems', JSON.stringify(updatedDeletedItems));
-
-		// setSelectedItems({}); // Tanlangan elementlarni tozalash
+		if (selectedItems > 0) {
+			axios.delete(`${apiUrl}admin/${selectedItems}`,)
+				.then((response) => {
+					console.log('Serverdan qaytgan javob:', response.data);
+				})
+				.catch((error) => {
+					console.error('Xatolik yuz berdi:', error);
+				});
+		} else {
+			alert('Please select an item to delete.');
+		}
 	};
+	// ------ ******** delet items ******* ------ //
 
+	// ------ ******** select items ******* ------ //
 	const toggleSelectItem = (id) => {
-		// Agar shu ID allaqachon tanlangan bo'lsa, uni bekor qilish (hech narsani tanlamaslik).
 		if (selectedItems === id) {
 			setSelectedItem(null);
 		} else {
-			// Aks holda, yangi tanlangan item'ni saqlash.
 			setSelectedItem(id);
 		}
 	};
+	// ------ ******** select items ******* ------ //
 
 
 
-	const isAllSelected =
-		currentItems.length > 0 && currentItems.every((item) => selectedItems[item.id]);
+
+
 
 
 	// ------ ****** pagination ****** -------- //
@@ -135,6 +104,19 @@ const ItemList = () => {
 		return <div>Error: {error}</div>;
 	}
 	// ------ ****** pagination ****** -------- //
+
+	// ------- ******* addItem ******* ------- //
+
+	const addItem = (data) => {
+		console.log(data);
+		axios.post(`${apiUrl}admin/`, data)
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.error('Xatolik yuz berdi:', error);
+			});
+	};
 
 
 	return (
@@ -160,7 +142,6 @@ const ItemList = () => {
 							type="text"
 							className="w-[500px] outline-none px-3 py-3 rounded-xl"
 							placeholder="Search"
-							onChange={handleSearchChange}
 						/>
 						<button className="px-4 py-2.5  text-white border-[2px] border-solid border-white rounded-xl">
 							Search
@@ -173,7 +154,7 @@ const ItemList = () => {
 							<button onClick={handleAddItem} className="border-[#F46A06] hover:bg-[#F46A06] m-1 hover:text-white hover:transition-all border-2 py-2 px-3 rounded-md">
 								Add new item
 							</button>
-							{showModal && <ItemListNew closeModal={handleCloseModal} />}
+							{showModal && <ItemListNew closeModal={handleCloseModal} addItem={addItem} />}
 							<button
 								onClick={handleDeleteSelectedItems}
 
--- src/pages/app/admin/restourantAdmin/components/modal.jsx
+++ src/pages/app/admin/restourantAdmin/components/modal.jsx
@@ -1,34 +1,109 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { apiUrl } from "../../../../../Api";
 
 const ItemListNew = ({ closeModal, addItem }) => {
-  
+  const [itemid, setitemid] = useState(0);
+  const [itemsCount, setItemsCount] = useState(0);
+
+  useEffect(() => {
+    // Asinxron funksiya yaratamiz
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}admin/`);
+        const id = response.data.length
+        setitemid(id)
+      } catch (error) {
+        console.error('Xatolik yuz berdi:', error);
+      }
+    };
+
+    // Yaratilgan asinxron funksiyani chaqiramiz
+    fetchData();
+  }, []);
+
+  const [item, setItem] = useState({
+    name: '',
+    category: '',
+    price: '',
+    quantity: '',
+    description: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setItem((prevItem) => ({
+      ...prevItem,
+      [name]: value,
+    }));
+  };
+
+  const handleSave = () => {
+    const newItem = { ...item, "id": itemsCount + 1 }; // Yangi `id`ni hisoblash
+    addItem(newItem);
+    closeModal();
+  };
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
       <div className="relative top-10 mx-auto p-5 border w-2/6 shadow-lg rounded-md bg-white">
         <div className="mt-8">
-          <label htmlFor="name">Item name</label>
-          <input type="text" name="name" placeholder="Item Name" className="mt-1 mb-3 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none" />
-          <label htmlFor="quanlity">Item quanlity</label>
-          <input type="number" name="quantity" placeholder="Item Quantity" className="mt-1 mb-3 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none" />
-          <label htmlFor="price">Item price</label>
-          <input type="number" name="price" placeholder="Item Price" className="mt-1 mb-3 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none" />
-          <label htmlFor="description">Item description</label>
-          
-          <textarea name="description" placeholder="Item Description"  rows="4" className="mt-1 mb-3 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none"></textarea>
-          <label htmlFor="category">Item category</label>
-          <select name="category" className="mt-1 mb-3 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none">
+          <input
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Item Name"
+            className="mt-1 mb-3 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none"
+            value={item.name}
+            onChange={handleChange}
+          />
+          <input
+            id="quantity"
+            type="number"
+            name="quantity"
+            placeholder="Item Quantity"
+            className="mt-1 mb-3 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none"
+            value={item.quantity}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="price"
+            placeholder="Item Price"
+            className="mt-1 mb-3 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none"
+            value={item.price}
+            onChange={handleChange}
+          />
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Item Description"
+            rows="4"
+            className="mt-1 mb-3 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none"
+            value={item.description}
+            onChange={handleChange}
+          ></textarea>
+          <select
+            id="category"
+            name="category"
+            className="mt-1 mb-3 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none"
+            value={item.category}
+            onChange={handleChange}
+          >
             <option value="">Select Category</option>
-            <option value="books">Coffee</option>
-            <option value="electronics">Tea</option>
-          </select> 
-          <button className="px-4 py-2 bg-orange-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-orange-600 focus:outline-none">
+            <option value="coffee">Coffee</option>
+            <option value="tea">Tea</option>
+          </select>
+          <button
+            onClick={handleSave}
+            className="px-4 py-2 bg-orange-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-orange-600 focus:outline-none"
+          >
             Save
           </button>
         </div>
         <div className="absolute flex justify-between w-full top-0 right-0 px-4 py-3">
           Add new item
-          <svg onClick={closeModal} className=" cursor-pointer fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+          <svg onClick={closeModal} className="cursor-pointer fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
             <path d="M16.2 1.8l-7.2 7.2 7.2 7.2-1.4 1.4-7.2-7.2-7.2 7.2-1.4-1.4 7.2-7.2-7.2-7.2 1.4-1.4 7.2 7.2 7.2-7.2 1.4 1.4z" />
           </svg>
         </div>
