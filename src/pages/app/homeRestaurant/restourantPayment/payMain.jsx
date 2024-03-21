da5f659db40e678da02dde6f891c54d1cc208471
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-03-21 11:55:52 +0500

    Navdata ga tegmanglar

--- src/pages/app/homeRestaurant/restourantPayment/payMain.jsx
+++ src/pages/app/homeRestaurant/restourantPayment/payMain.jsx
@@ -8,7 +8,7 @@ import { FiLogIn } from "react-icons/fi";
 
 const navdata = [
 	{ id: 1, icon: <FaHome color='black' />, name: 'Home', to: "/", isActive: false },
-	{ id: 4, icon: <FiLogIn color='black' />, name: 'Restaurant', to: "/homerestourant", isActive: true },
+	{ id: 4, icon: <FiLogIn color='black' />, name: 'Restaurant', to: "/homerestourant", isActive: true},
 	{ id: 5, icon: <FiLogIn color='black' />, name: 'hotel', to: "/hotel", isActive: false },
 	{ id: 3, icon: <IoCall color='black' />, name: 'ContactUs', to: "", isActive: false },
 	{ id: 7, icon: <FiLogIn color='black' />, name: 'Login', to: "/loginSignUp", isActive: false }
